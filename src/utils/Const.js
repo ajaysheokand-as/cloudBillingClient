39a014643544d28a935bbda3279a13a1a83d79fa
ajaysheokand-as <ajaysheokand.as@gmail.com>
2024-08-11 13:01:24 +0530

    url update

--- src/utils/Const.js
+++ src/utils/Const.js
@@ -1,3 +1,4 @@
- export const baseUrl = "https://cloudbillingbeckend.onrender.com/";
+//  export const baseUrl = "https://cloudbillingbeckend.onrender.com/";
 
-  //export const baseUrl = "http://localhost:5000/";
+//   export const baseUrl = "http://localhost:4000/";
+  export const baseUrl = "http://23.142.25.32:4000/";
